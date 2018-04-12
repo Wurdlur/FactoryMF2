84d5a1f9766ee5f8bbf9791c2bf9a20d23afca17
Harrisen Sanchez <hmsanchez98@gmail.com>
2018-04-12 14:41:40 -0600

    yeet

--- FactoryMF/FactoryMF/MainWindow.xaml.cs
+++ FactoryMF/FactoryMF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FactoryMF
         {
             InitializeComponent();
         }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
