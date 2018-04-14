c588caf0650c00ac8fa4a5ad8da2dad9adac52ac
Harrisen Sanchez <hmsanchez98@gmail.com>
2018-04-13 18:14:45 -0600

    rest of elements and remove btn
    hi

--- FactoryMF/FactoryMF/MainWindow.xaml.cs
+++ FactoryMF/FactoryMF/MainWindow.xaml.cs
@@ -27,6 +27,17 @@ namespace FactoryMF
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
+            if(csharp.IsChecked == true)
+            {
+                //kill myself
+            }else if(html.IsChecked == true)
+            {
+                //kill urself
+            }
+            else
+            {
+                //kill everyone
+            } 
 
         }
 
@@ -34,24 +45,49 @@ namespace FactoryMF
         {
             TextBox tb = new TextBox();
             tb.Text = textBoxContent.Text;
-            tb.Height = textBoxHeight.Height;
-            tb.Width = textBoxWidth.Width;
-            
+            tb.Height = double.Parse(textBoxHeight.Text);
+            tb.Width = double.Parse(textBoxWidth.Text);
+            tb.Margin = new Thickness(double.Parse(textBoxLeft.Text), double.Parse(textBoxTop.Text), 0, 0);
+            OurListBox.Items.Add(tb);
+
         }
 
         private void addCircle_Click(object sender, RoutedEventArgs e)
         {
-
+            Ellipse c = new Ellipse();
+            //c.text
+            c.Stroke = Brushes.Red;
+            c.StrokeThickness = 5;
+            c.Height = double.Parse(circleHeight.Text);
+            c.Width = double.Parse(circleWidth.Text);
+            c.Margin = new Thickness(double.Parse(circleLeft.Text), double.Parse(circleTop.Text), 0, 0);
+            OurListBox.Items.Add(c);
         }
 
         private void addLabel_Click(object sender, RoutedEventArgs e)
         {
-
+            Label tb = new Label();
+            tb.Content = labelContent.Text;
+            tb.Height = double.Parse(labelHeight.Text);
+            tb.Width = double.Parse(labelWidth.Text);
+            tb.Margin = new Thickness(double.Parse(labelLeft.Text), double.Parse(labelTop.Text), 0, 0);
+            OurListBox.Items.Add(tb);
         }
 
         private void addRectangle_Click(object sender, RoutedEventArgs e)
         {
+            Rectangle tb = new Rectangle();
+            //tb.Text = textBoxContent.Text;
+            tb.Fill = Brushes.Black;
+            tb.Height = double.Parse(rectangleHeight.Text);
+            tb.Width = double.Parse(rectangleWidth.Text);
+            tb.Margin = new Thickness(double.Parse(rectangleLeft.Text), double.Parse(rectangleTop.Text), 0, 0);
+            OurListBox.Items.Add(tb);
+        }
 
+        private void removeButton_Click(object sender, RoutedEventArgs e)
+        {
+            OurListBox.Items.RemoveAt(0);
         }
     }
 }
